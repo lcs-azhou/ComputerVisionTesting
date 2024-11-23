a0bf28fb6947ce1ee4785c4d255b0a0014d3597c
Ansheng Zhou <azhou@lcs.on.ca>
2024-11-23 11:02:58 -0500

    Initial Commit

---
+++ ComputerVisionTesting/ComputerVisionTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputerVisionTestingApp.swift
+//  ComputerVisionTesting
+//
+//  Created by Ansheng Zhou on 2024-11-23.
+//
+
+import SwiftUI
+
+@main
+struct ComputerVisionTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
